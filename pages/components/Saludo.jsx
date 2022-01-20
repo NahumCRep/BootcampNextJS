e1cb62999a1503acda108d0018269113d214a496
Nahum Casco <nahumcasco.v@gmail.com>
2022-01-20 09:43:38 -0500

    first component

---
+++ pages/components/Saludo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Saludo = () => {
+  return(
+      <div>
+          <h1>Hola!</h1>
+          <p>Presiona para mostrar el contenido</p>
+      </div>
+  )
+}
+
+export default Saludo;
